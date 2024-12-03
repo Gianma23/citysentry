4449d21574cba996bb96201f9bc30749c38fdd0f
Gianmaria Saggini <gianmaria.saggini@gmail.com>
2024-12-03 23:21:38 +0100

    small fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    "projectId":"city-sentry",
+    "appId":"1:913517777208:web:88ddea034d623525ddb1cd",
+    "storageBucket":"city-sentry.firebasestorage.app",
+    "apiKey":"AIzaSyAD3_ptLdz6PgkEC8ZtXLO82txLnASVbVU",
+    "authDomain":"city-sentry.firebaseapp.com",
+    "messagingSenderId":"913517777208",
+    "measurementId":"G-WMN3L6RS3S"
+  }
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { importProvidersFrom } from '@angular/core';
+import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -15,18 +24,8 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideHttpClient(),
-    provideRouter(routes, withPreloading(PreloadAllModules)), 
-      provideFirebaseApp(() => 
-        initializeApp({
-          "projectId":"city-sentry",
-          "appId":"1:913517777208:web:88ddea034d623525ddb1cd",
-          "storageBucket":"city-sentry.firebasestorage.app",
-          "apiKey":"AIzaSyAD3_ptLdz6PgkEC8ZtXLO82txLnASVbVU",
-          "authDomain":"city-sentry.firebaseapp.com",
-          "messagingSenderId":"913517777208",
-          "measurementId":"G-WMN3L6RS3S"
-        })
-    ), 
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() => initializeApp({ ...environment.firebaseConfig })),
     provideFirestore(() => getFirestore()),
   ],
 });
